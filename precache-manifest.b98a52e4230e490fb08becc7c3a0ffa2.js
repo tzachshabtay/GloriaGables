03f58853330a15fe61c27d16d3d518c566bfa8a9
Tzach Shabtay <tzachshabtay@gmail.com>
2019-12-17 23:39:13 -0500

    Updates

---
+++ precache-manifest.b98a52e4230e490fb08becc7c3a0ffa2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629fc120b3cf4e8ba3dfd09b7c492667",
+    "url": "/GloriaGables/index.html"
+  },
+  {
+    "revision": "0a4e0a3ec611db3ab43b",
+    "url": "/GloriaGables/static/css/2.62027c08.chunk.css"
+  },
+  {
+    "revision": "1f17ea0d97ce5702947f",
+    "url": "/GloriaGables/static/css/main.1f85ac99.chunk.css"
+  },
+  {
+    "revision": "0a4e0a3ec611db3ab43b",
+    "url": "/GloriaGables/static/js/2.58a77d66.chunk.js"
+  },
+  {
+    "revision": "1f17ea0d97ce5702947f",
+    "url": "/GloriaGables/static/js/main.2b30f54b.chunk.js"
+  },
+  {
+    "revision": "bc6ca34ed7e413b7632d",
+    "url": "/GloriaGables/static/js/runtime-main.9a4beedc.js"
+  },
+  {
+    "revision": "facaadc7e6fb7969d4e52b2e0398ca27",
+    "url": "/GloriaGables/static/media/events.facaadc7.md"
+  },
+  {
+    "revision": "d07f471e07cd65d7ac67873e0641a005",
+    "url": "/GloriaGables/static/media/history.d07f471e.md"
+  },
+  {
+    "revision": "1d82d0659088beca7041248c8578f2c5",
+    "url": "/GloriaGables/static/media/lincoln.1d82d065.md"
+  },
+  {
+    "revision": "9b82265d87940efe88ce2d147baade89",
+    "url": "/GloriaGables/static/media/news.9b82265d.md"
+  },
+  {
+    "revision": "bb49d4d518bfb20a52d82b7abbe7c68f",
+    "url": "/GloriaGables/static/media/privacy.bb49d4d5.md"
+  },
+  {
+    "revision": "df4267fbbd9c3310bd28bbeb609d69e3",
+    "url": "/GloriaGables/static/media/west-side.df4267fb.md"
+  }
+]);
